4098dd419266f54815c0e38ad226c0f2e7fe161d
jinhothree <60808225+jinhothree@users.noreply.github.com>
2020-03-21 23:33:06 -0700

    Add files via upload

--- Desktop/GitRepo/Python_challenge/PyPoll/main.py
+++ Desktop/GitRepo/Python_challenge/PyPoll/main.py
@@ -1,70 +1,39 @@
 import os
 import csv
 from operator import itemgetter 
-vote_list = []
+total_vote_count = 0
 candidate_list = []
 row_count = 0
-khan_vote = []
-correy_vote = []
-li_vote = []
-tool_vote = []
-#Set up the csv reader
+candidate ={}
+percent = {}
 csvpath = os.path.join("election_data.csv")
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     next(csvreader, None)
     for row in csvreader:
-#Count the total number of votes
-        vote_list.append(row[1])
-#Store all the values in the "Candidate" into a list
+        total_vote_count += 1
         candidate_list.append(row[2])
-#Store vote count for each candidates
-        if row[2] == "Correy":
-            correy_vote.append(row[2])
-        elif row[2] == "Khan":
-            khan_vote.append(row[2])
-        elif row[2] == "Li":
-            li_vote.append(row[2])
-        elif row[2] == "O'Tooley":
-            tool_vote.append(row[2])
-#Find the names of the candidates
-candidates = set(candidate_list)
-sort_candidates = sorted(candidates)
-#Find the total number of votes
-total_vote = len(vote_list)
-#Find the vote count for each candidate
-khan_total = len(khan_vote)
-correy_total = len(correy_vote)
-li_total = len(li_vote)
-tool_total = len(tool_vote)
-#Find the percentage of votes gained for each candidates
-khan_per = round((khan_total/total_vote)*100,2)
-correy_per = round((correy_total/total_vote)*100, 2)
-li_per = round((li_total/total_vote)*100,2)
-tool_per = round((tool_total/total_vote)*100,2)
-#Find the winner
-tally = [{"Name":"Correy" , "Vote":correy_total},{"Name":"Khan" , "Vote": khan_total},
-{"Name":"Li" , "Vote":li_total},{"Name":"O'Tooley" , "Vote":tool_total}]
-sort_tally = sorted(tally, key=itemgetter('Vote'),reverse = True) 
-winner = sort_tally[0]
-#Printing the total vote
-print("Total Votes: ", total_vote)
-#Printing the summary table. 
-# a) Can I sort it the table by the most popular vote?
-# b) Is this hard coding????
-print(sort_candidates[0],':', correy_per,'%', '(',correy_total, ')')
-print(sort_candidates[1],':', khan_per,'%','(', khan_total, ')')
-print(sort_candidates[2], ':',li_per,'%', '(',li_total, ')')
-print(sort_candidates[3], ':',tool_per ,'%','(',tool_total, ')')
-print("Winner: ", winner)
-#Write the result on a text file
-output_path = os.path.join("analysis.txt")
+        if row[2] not in candidate:
+            candidate[row[2]] = 1
+        else:
+            candidate[row[2]] = candidate[row[2]] + 1
+    candidate = {name: float(vote) for name, vote in candidate.items()}
+    total_vote_count = float(total_vote_count)
+    for vote in candidate:
+        cand_per = round((candidate[vote]/total_vote_count)*100,2)
+        percent[vote] =  cand_per
+    winner = max(candidate, key=candidate.get)
+    unique_cand_name = set(candidate_list)
+    sort_candidates = sorted(unique_cand_name)
+    print("Total Vote: ", total_vote_count)
+    for name in sort_candidates:
+        print(name, percent[name],'%', candidate[name])
+    print('Winner is ', winner)
+    output_path = os.path.join("analysis.txt")
 with open(output_path, "w+") as textfile:
     textfile.writelines("Election Results" + "\n")
     textfile.writelines("________________________" + "\n")
-    textfile.writelines(sort_candidates[0]+':'+ str(correy_per)+'%'+ '('+str(correy_total)+')'+ "\n")
-    textfile.writelines(sort_candidates[1]+':'+ str(khan_per)+'%'+'('+ str(khan_total)+ ')'+ "\n")
-    textfile.writelines(sort_candidates[2]+ ':'+str(li_per)+'%'+ '('+str(li_total)+ ')'+ "\n")
-    textfile.writelines(sort_candidates[3]+ ':'+str(tool_per) +'%'+'('+str(tool_total)+')'+ "\n")
+    for name in sort_candidates:
+        textfile.writelines(name +' '+ str(percent[name])+'%'+" "+ str(candidate[name])+'\n')
     textfile.writelines("________________________" + "\n")
     textfile.writelines("Winner: "+ str(winner) + "\n")
